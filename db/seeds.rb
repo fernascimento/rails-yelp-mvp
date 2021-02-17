39fd5f3b98f07515912d46d0fddb4e7689244af4
Fernando Nascimento <vashoo@hotmail.com>
2021-02-17 16:52:10 +0100

    Seeding is done

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,23 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require 'faker'
+
+Restaurant.destroy_all
+
+puts "Creating restaurants..."
+
+category_array = ["chinese", "italian", "japanese", "french", "belgian"]
+
+10.times do 
+  restaurant = Restaurant.new( name: Faker::Restaurant.name,
+    address: Faker::Address.full_address,
+    phone_number: Faker::PhoneNumber,
+    category: category_array.sample )
+    restaurant.save
+
+    puts "#{restaurant.name} created"
+end
+
+puts "Seeding done"
